c1dc628aa8ac2f21b4d4e5e964e1d9e9c4cd9cbd
mihirsingh1001 <mihirbais16@gmail.com>
2024-06-26 22:38:16 +0530

    Electronic Store Setup

---
+++ ElectronicStore/src/main/java/com/mihirsingh/electronicstore/ElectronicStoreApplication.java
@@ -0,0 +1,14 @@
+package com.mihirsingh.electronicstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronicStoreApplication.class, args);
+		System.out.println("Running");
+	}
+
+}
